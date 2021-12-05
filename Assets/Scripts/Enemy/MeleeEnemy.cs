35942859c75ddc1cecd0e97ee5a6769c4f07979c
Pandemonium Games <nick.bota23@gmail.com>
2021-12-06 00:21:35 +0200

    Melee enemy fix

--- Assets/Scripts/Enemy/MeleeEnemy.cs
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MeleeEnemy : MonoBehaviour
 {
-    [Header ("Attack Parameters")]
+    [Header("Attack Parameters")]
     [SerializeField] private float attackCooldown;
     [SerializeField] private float range;
     [SerializeField] private int damage;
@@ -14,9 +14,6 @@ public class MeleeEnemy : MonoBehaviour
     [Header("Player Layer")]
     [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer = Mathf.Infinity;
-
-    [Header("Attack Sound")]
-    [SerializeField] private AudioClip attackSound;
 
     //References
     private Animator anim;
@@ -36,11 +33,10 @@ public class MeleeEnemy : MonoBehaviour
         //Attack only when player in sight?
         if (PlayerInSight())
         {
-            if (cooldownTimer >= attackCooldown && playerHealth.currentHealth > 0)
+            if (cooldownTimer >= attackCooldown)
             {
                 cooldownTimer = 0;
                 anim.SetTrigger("meleeAttack");
-                SoundManager.instance.PlaySound(attackSound);
             }
         }
 
@@ -50,7 +46,7 @@ public class MeleeEnemy : MonoBehaviour
 
     private bool PlayerInSight()
     {
-        RaycastHit2D hit = 
+        RaycastHit2D hit =
             Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
             0, Vector2.left, 0, playerLayer);
